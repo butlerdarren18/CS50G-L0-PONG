7d4222fe890bd1baf69c1f8aae5bfad737f535b4
Darren Butler <butlerdarren18@gmail.com>
2025-12-08 03:06:12 -0800

    added movement to the paddles

--- main.lua
+++ main.lua
@@ -6,6 +6,7 @@ push = require 'push' -- this is how you add a library (remember to put it insid
 WINDOW_WIDTH  = 1280
 WINDOW_HEIGHT = 720 
 
+PADDLE_SPEED = 200
 -- PUSH ALLOWS FOR VIRTUAL RESOLUTION WINDOW 
 
 VIRTUAL_WIDTH  = 432
@@ -13,7 +14,14 @@ VIRTUAL_HEIGHT = 243
 
 function love.load()
 	gameFont = love.graphics.newFont('Font.ttf', 25)
+	scoreFont = love.graphics.newFont('Font.ttf', 15)
 	love.graphics.setFont(gameFont)
+
+	player1Score = 0 
+	player2Score = 0 
+
+	player1Y = 30
+	player2Y = VIRTUAL_HEIGHT - 50
 
 	love.graphics.setDefaultFilter('nearest', 'nearest')
 
@@ -21,12 +29,28 @@ function love.load()
 		fullscreen = false,  -- remember your commas in your tables :| 
 		resizable = false,
 		vsync = true
+
+	
 	})
 	
 end
 
--- love.graphics.setDefaultFilter(min, mag)
--- love.event.quit()
+
+function love.update(delta)
+	-- PLAYER 1 MOVEMENT
+	if love.keyboard.isDown('w') then 
+		player1Y = player1Y + -PADDLE_SPEED * delta
+	elseif love.keyboard.isDown('s') then
+		player1Y = player1Y + PADDLE_SPEED * delta 
+	end 
+	-- PLAYER 2 MOVEMENT 
+	if love.keyboard.isDown('up') then 
+		player2Y = player2Y + -PADDLE_SPEED * delta
+	elseif love.keyboard.isDown('down') then 
+		player2Y = player2Y + PADDLE_SPEED * delta
+	end
+	
+end 
 
 -- if escape key is pressed then quit the program
 function love.keypressed(key)
@@ -44,8 +68,8 @@ function love.draw()
 	love.graphics.printf('HELLO PONG', 0,VIRTUAL_HEIGHT / 10, VIRTUAL_WIDTH, 'center')
 	
 	-- render the paddles 
-	love.graphics.rectangle('fill', 10, 30, 5, 20) -- left 
-	love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT  - 50, 5, 20) -- right 
+	love.graphics.rectangle('fill', 10, player1Y, 5, 20) -- left 
+	love.graphics.rectangle('fill',VIRTUAL_WIDTH-10,player2Y, 5, 20) -- right 
 
 	-- render the ball 
 	love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 -2 , VIRTUAL_HEIGHT / 2 - 2, 4,4)
