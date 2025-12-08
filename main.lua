550b76e8590ec7cd6883feed506ea24d45c2da22
Darren Butler <butlerdarren18@gmail.com>
2025-12-08 13:56:50 -0800

    added ball velocity and paddles are now confined to the screen

--- main.lua
+++ main.lua
@@ -13,6 +13,9 @@ VIRTUAL_WIDTH  = 432
 VIRTUAL_HEIGHT = 243
 
 function love.load()
+	-- seed the rng with current time 
+	math.randomseed(os.time())
+
 	gameFont = love.graphics.newFont('Font.ttf', 8)
 	scoreFont = love.graphics.newFont('Font.ttf', 16)
 	love.graphics.setFont(gameFont)
@@ -22,6 +25,13 @@ function love.load()
 
 	player1Y = 30
 	player2Y = VIRTUAL_HEIGHT - 50
+
+	ballX = VIRTUAL_WIDTH / 2 - 2 
+	ballY = VIRTUAL_HEIGHT / 2 - 2
+
+	ballDX  = math.random(2) == 1 and 100 or -100 
+	ballDY = math.random(-50, 50)
+	gameState = 'start'
 
 	love.graphics.setDefaultFilter('nearest', 'nearest')
 
@@ -37,26 +47,51 @@ end
 
 
 function love.update(delta)
+
 	-- PLAYER 1 MOVEMENT
 	if love.keyboard.isDown('w') then 
-		player1Y = player1Y + -PADDLE_SPEED * delta
+		player1Y = math.max(0, player1Y + -PADDLE_SPEED * delta) 
 	elseif love.keyboard.isDown('s') then
-		player1Y = player1Y + PADDLE_SPEED * delta 
+		player1Y = math.min(VIRTUAL_HEIGHT -20, player1Y + PADDLE_SPEED * delta )
+
 	end 
+
 	-- PLAYER 2 MOVEMENT 
 	if love.keyboard.isDown('up') then 
-		player2Y = player2Y + -PADDLE_SPEED * delta
+		player2Y = math.max(0,player2Y + -PADDLE_SPEED * delta)
 	elseif love.keyboard.isDown('down') then 
-		player2Y = player2Y + PADDLE_SPEED * delta
+		player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * delta)
 	end
 	
+	-- BALL 
+	if gameState == 'play' then 
+		ballX = ballX + ballDX * delta 
+		ballY = ballY + ballDY * delta 
+	end 
+
+
 end 
 
 -- if escape key is pressed then quit the program
 function love.keypressed(key)
 	if key == 'escape' then 
 		love.event.quit()
+	elseif key == 'enter'or  key == 'return' then 
+		if gameState == 'start' then 
+			gameState = 'play'
+		else
+			gameState = 'start'
+			-- Set ball back to center 
+			ballX = VIRTUAL_WIDTH / 2 - 2 
+			ballY = VIRTUAL_HEIGHT / 2 - 2 
+
+			-- ternary operation --- if 1 then 100 and if not 1 then -100 
+			ballDX = math.random(2) == 1 and 100 or -100 
+			ballDY = math.random(-50, 50) * 1.5
+		end 
+
 	end
+
 end 
 
 function love.draw()
@@ -72,17 +107,16 @@ function love.draw()
 	love.graphics.rectangle('fill',VIRTUAL_WIDTH-10,player2Y, 5, 20) -- right 
 
 	-- render the ball 
-	love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 -2, VIRTUAL_HEIGHT / 2 -2, 4,4)
+	love.graphics.rectangle('fill', ballX, ballY, 4,4)
 
 	-- switch font and draw score to right and left center of screen
 	love.graphics.setFont(scoreFont)
-	love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT /3)
-	love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH /2 + 30, VIRTUAL_HEIGHT / 3) 
+	love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+	love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3) 
 	
 	push:apply('end')
+
+-- seeded random 
+-- applied math.max( ) and math.min( ) to playerY values to keep the paddles on screen 
 end
-
-
--- added font (also changed to more appropriate one)
--- drew the paddles and ball to screen 
--- changed where the printf is on the yaxis 
+ 
